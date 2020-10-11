cab62407522f9f66164f154408165e77afe9d7b5
Slad3 <feba23@live.com>
2020-10-10 23:33:06 -0500

    Added stuff to SigmoidFormula.py

--- flaskr/Messages/SigmoidFormula.py
+++ flaskr/Messages/SigmoidFormula.py
@@ -2,13 +2,11 @@ from math import e
 import matplotlib.pyplot as plt
 
 maxHeight = 1.0
-hShift = 1
+hShift = 2
+stretch = 1
 
 def sigmoid(x):
-    return maxHeight/(1+e**(-x + hShift))
-
-def form(x):
-    return 13**x -1
+    return maxHeight/(1+e**(-stretch*x + hShift))
 
 
 if __name__ == '__main__':
@@ -17,10 +15,13 @@ if __name__ == '__main__':
 
 
     print('=========')
-    interval = 20
+    interval = 100
     for i in range(-interval, interval):
-        x = i/interval
+        x = i/20
         y = sigmoid(x)
 
-        plt.scatter(x, y)
+        plt.scatter(x, 1-y)
+    plt.xlim(-5, 5)
+    plt.ylim(0, 2)
+
     plt.show()
