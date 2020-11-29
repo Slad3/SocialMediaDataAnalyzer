ecca5d4d89f8824951d7abc823c47376b6866f7b
Ben <feba23@live.com>
2020-11-28 22:37:48 -0600

    Updating readme

--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo cp SocialMediaDataAnalyzer.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl restart apache2
-sudo systemctl start SocialMediaDataAnalyzer.service
-sudo systemctl status SocialMediaDataAnalyzer.service
